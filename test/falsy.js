3a46c81d39b5f94c0c17c47638939af2528520f3
Terin Stock <terinjokes@gmail.com>
2015-03-30 15:47:23 -0700

    Bump to 1.0.0 + newer tape

--- test/falsy.js
+++ test/falsy.js
@@ -1,7 +1,7 @@
 var test = require('tape');
 var defined = require('../');
 
-test(function (t) {
+test('falsy', function (t) {
     t.plan(1);
     var opts = { y : false, w : 4 };
     var x = defined(opts.x, opts.y, opts.w, 8);
