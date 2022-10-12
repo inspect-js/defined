84801c054280c86c433022be64feddf38ac426aa
Jordan Harband <ljharb@gmail.com>
2022-10-12 01:06:44 -0700

    [eslint] fix indentation

--- test/falsy.js
+++ test/falsy.js
@@ -4,8 +4,8 @@ var test = require('tape');
 var defined = require('../');
 
 test('falsy', function (t) {
-    t.plan(1);
-    var opts = { y: false, w: 4 };
-    var x = defined(opts.x, opts.y, opts.w, 8);
-    t.equal(x, false);
+	t.plan(1);
+	var opts = { y: false, w: 4 };
+	var x = defined(opts.x, opts.y, opts.w, 8);
+	t.equal(x, false);
 });
